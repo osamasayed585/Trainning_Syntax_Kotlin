511981da51c6ad071f3c5fc27fe765e43e1bddf2
Osama Sayed <osamasayed151@gmail.com>
2021-11-20 22:54:33 +0200

    Training on list & MutableList

--- src/list/MutableList.kt
+++ src/list/MutableList.kt
@@ -1,7 +1,21 @@
 package list
 
-fun main() {
-    val infoList = mutableListOf("Hello","OSama", "Sayed", "and", "Welcome","to","Kotiln")
-    infoList[3]= "&"
-    println(infoList)
+fun main() {   //    index    0      1        2        3      4         5     6
+    val infoList = listOf("Hello", "OSama", "Sayed", "and", "Welcome", "to", "Kotiln")
+
+    // for loop with list in kotlin
+
+for (item in infoList){ println(item)}
+
+//    for (item in 0 until infoList.size ){ println(infoList[item])}
+
+//    for (item in 0..infoList.size -1) { println(infoList[item]) }
+
+//    for (element in 0..10 step 2) { println(element) }
+
+// for (element in 10.downTo(0) step 2){ println(element) }
+
+//  for (element in 10.downTo(5)){ println(element) }
+
+// for (element in 0..10){ println(element) }
 }
