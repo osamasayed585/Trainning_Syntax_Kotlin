5b81d5fcc83c138e6b79a1d1127044bcc61e3072
Osama Sayed <osamasayed151@gmail.com>
2021-11-28 16:34:58 +0200

    add new topic "Access Modifier"

--- src/access_modifiers/A.kt
+++ src/access_modifiers/A.kt
@@ -3,11 +3,12 @@ package access_modifiers
 class A {
 
     var age: Int = 0
-        set(value) {
-        field = if (value in 1..49){
-            value
-        }else{
-            0
-        }
-    }
+//        set(value) {
+//        field = if (value in 1..60){
+//            value
+//        }else{
+//            0
+//        }
+//    }
+    private set
 }
--- src/access_modifiers/TheMain.kt
+++ src/access_modifiers/TheMain.kt
@@ -3,6 +3,6 @@ package access_modifiers
 fun main() {
     val obj = A()
     println(obj.age)
-    obj.age = 60
+ //   obj.age = 60
     println(obj.age)
 }
