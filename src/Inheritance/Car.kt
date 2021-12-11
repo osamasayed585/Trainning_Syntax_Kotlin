a9207d56b596399826dce2e15777710e8b15019d
Osama Sayed <osamasayed151@gmail.com>
2021-12-11 14:35:59 +0200

    add new topic "Inheritance"

--- src/Inheritance/Car.kt
+++ src/Inheritance/Car.kt
@@ -2,10 +2,16 @@ package Inheritance
 
 open class Car(var model: String) {
 
-    var getCarCount = 0
+    val listName =  mutableListOf<String>()
+
+    var getNameCount = 0
     private set
+    get() = listName.size
 
     fun getModel(){
         println("$model")
+    }
+    fun getName(name: String){
+        listName.add(name)
     }
 }
--- src/Inheritance/mainTest.kt
+++ src/Inheritance/mainTest.kt
@@ -3,6 +3,12 @@ package Inheritance
 fun main() {
 
     val testObj = Gallery(model = "x6",welcome = "Hello dear")
+
     testObj.getModel()
     testObj.sayHelloCar()
+
+    testObj.getName("One")
+    testObj.getName("Two")
+    testObj.getName("Three")
+    println(testObj.getNameCount)
 }
